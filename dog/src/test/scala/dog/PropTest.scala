3309f3b753263f03f28c21413e9ffc5e13885509
pocketberserker <pocketberserker@gmail.com>
2015-08-26 04:39:15 +0900

    fix typo

--- dog/src/test/scala/dog/PropTest.scala
+++ dog/src/test/scala/dog/PropTest.scala
@@ -3,7 +3,7 @@ package dog
 import scalaprops._
 import props._
 
-object Proptest extends Scalaprops {
+object PropTest extends Scalaprops {
 
   val convertTestCase = Property.property((i : Int) => TestCase.ok(i).to)
 
