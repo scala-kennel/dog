6f634cb613094a993380dfec26ac8794827e9859
pocketberserker <pocketberserker@gmail.com>
2015-08-25 23:26:36 +0900

    add helper function for Param

--- dog/src/main/scala/dog/Dog.scala
+++ dog/src/main/scala/dog/Dog.scala
@@ -1,8 +1,16 @@
 package dog
 
+import java.util.concurrent.TimeUnit
+
 trait Dog {
 
   def param: Param = Param.default
 
   def listener: DogListener = DogListener.default
+
+  implicit class ParamSyntax[A] private[dog](val self: TestCase[A]) {
+
+    def timeout(n: Int, timeunit: TimeUnit): TestCase[A] =
+      self.local(p => Param.timeout(n, timeunit)(p))
+  }
 }
