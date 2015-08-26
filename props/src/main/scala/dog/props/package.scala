93b22222b6179a1f082a6bdedc09e31b48d86547
pocketberserker <pocketberserker@gmail.com>
2015-08-27 00:10:37 +0900

    add Property#toTestCase

--- props/src/main/scala/dog/props/package.scala
+++ props/src/main/scala/dog/props/package.scala
@@ -2,6 +2,8 @@ package dog
 
 import scalaz._
 import scalaprops._
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.TimeoutException
 
 package object props {
 
@@ -31,5 +33,37 @@ package object props {
   implicit class AssertionResultSynax[A](val self: AssertionResult[A]) {
 
     def to: Property = assertion.asProperty(self)
+  }
+
+  private[this] def checkResultToTestResult[A](result: CheckResult[A], value: A): TestResult[A] = result match {
+    case _: CheckResult.Proven | _: CheckResult.Passed => TestResult(value)
+    case _: CheckResult.Exhausted | _: CheckResult.Falsified =>
+      TestResult.nel(-\/(NotPassedCause.violate(result.toString)), List())
+    case e: CheckResult.GenException =>
+      TestResult.error(List(e.exception), List())
+    case e: CheckResult.PropException =>
+      TestResult.error(List(e.exception), List(NotPassedCause.violate(e.toString)))
+    case e: CheckResult.Timeout =>
+      TestResult.error(List(), List(NotPassedCause.violate(e.toString)))
+    case e: CheckResult.Ignored =>
+      TestResult.nel(-\/(NotPassedCause.skip(e.reason)), List())
+  }
+
+  implicit class PropertySyntax(val self: Property) {
+
+    def toTestCase(param: scalaprops.Param = scalaprops.Param.withCurrentTimeSeed()): TestCase[Unit] =
+      Kleisli.kleisli((paramEndo: Endo[dog.Param]) => try {
+        val p = paramEndo(dog.Param.default)
+        val cancel = new AtomicBoolean(false)
+        checkResultToTestResult((p.executorService match {
+          case Some(s) => Task(self.check(param, cancel, count => ()))(s)
+          case None => Task(self.check(param, cancel, count => ()))
+        }).runFor(param.timeout))
+      } catch {
+        case e: TimeoutException => TestResult.error(List(e), List())
+        case e: Throwable => TestResult.error(List(e), List())
+      } finally {
+        cancel.set(true)
+      })
   }
 }
