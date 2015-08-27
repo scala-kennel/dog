80e87523a292269aed2bd052446cf3e4501560b4
pocketberserker <pocketberserker@gmail.com>
2015-08-27 16:23:47 +0900

    public

--- core/src/main/scala/dog/Param.scala
+++ core/src/main/scala/dog/Param.scala
@@ -6,7 +6,7 @@ import scalaz.Endo
 
 final case class Param(
   timeout: Duration = Duration(30, TimeUnit.SECONDS),
-  private[dog] executorService: Option[ExecutorService] = None
+  executorService: Option[ExecutorService] = None
 )
 
 object Param {
@@ -16,7 +16,7 @@ object Param {
   def timeout(n: Int, timeunit: TimeUnit): Endo[Param] =
     Endo(_.copy(timeout = Duration(n, timeunit)))
 
-  private[dog] def executorService(service: ExecutorService): Endo[Param] =
+  def executorService(service: ExecutorService): Endo[Param] =
     Endo(_.copy(executorService = Some(service)))
 
   val id: Endo[Param] = Endo.idEndo[Param]
