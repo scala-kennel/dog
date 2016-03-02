ad415fe9b8396a28db2abe06942e41df9ab8742f
pocketberserker <pocketberserker@gmail.com>
2016-03-03 03:14:42 +0900

    scalaz 7.2.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._, Keys._
 object Dependencies {
 
   object Version {
-    val scalaz = "7.2.0"
+    val scalaz = "7.2.1"
     val scalaprops = "0.2.0"
     val testInterface = "1.0"
   }
