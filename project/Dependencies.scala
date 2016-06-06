206614c7ddda0a8bda38f464b5c24b2d6190b4f3
pocketberserker <pocketberserker@gmail.com>
2016-06-07 04:19:21 +0900

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,8 +3,8 @@ import sbt._, Keys._
 object Dependencies {
 
   object Version {
-    val scalaz = "7.2.2"
-    val scalaprops = "0.3.0"
+    val scalaz = "7.2.3"
+    val scalaprops = "0.3.1"
     val testInterface = "1.0"
   }
 
