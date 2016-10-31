e34cb3ec1dd5b32ecaca0cb722319b5507c1fdc5
xuwei-k <6b656e6a69@gmail.com>
2016-10-31 17:47:53 +0900

    support Scala 2.12.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,8 +3,8 @@ import sbt._, Keys._
 object Dependencies {
 
   object Version {
-    val scalaz = "7.2.4"
-    val scalaprops = "0.3.2"
+    val scalaz = "7.2.7"
+    val scalaprops = "0.3.4"
     val testInterface = "1.0"
   }
 
